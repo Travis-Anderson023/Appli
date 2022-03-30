b728ff47a95c78b88e0c45f68fef0d5fd6cb6a40
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-30 10:41:23 -0600

    moved apollo client from App.js to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,35 @@ import { App } from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -37,9 +66,11 @@ const theme = createTheme({
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
