f702ea1a4912a3ee3528e7efc4cbb52123d8ac08
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-30 10:41:31 -0600

    moved apollo client from App.js to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,6 @@
-import React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
 import { Box } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Account, CoverLetters, Home, Resumes, Signup } from './pages';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 export const App = () => {
   const styles = {
@@ -44,7 +14,6 @@ export const App = () => {
   }
 
   return (
-    <ApolloProvider client={client}>
       <Box sx={styles.wrapper}>
         <BrowserRouter>
           <Routes>
@@ -56,6 +25,5 @@ export const App = () => {
           </Routes>
         </BrowserRouter>
       </Box>
-    </ApolloProvider>
   );
 }
