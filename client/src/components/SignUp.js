9e454384a3abf2564a490a52a99b10237de6841c
Joe Lee <77361869+jollypong@users.noreply.github.com>
2022-03-30 22:56:57 -0600

    signup works! was able to sign up asdf and qwer

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,11 +1,50 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import { reStyles } from "../reusableStyles";
+// import { validate } from '../../../server/models/User';
 
 export const SignUp = (props) => {
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
+        // confirm_password: '' TRAVIS IT doesnt like the dash!!!
+    });
+
+    const [addUser] = useMutation(ADD_USER);
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <Box sx={{
             ...reStyles.flexContainer, bgcolor: 'primary.main', width: '100%', alignItems: 'center',
-        }}>
+        }} component='form'>
             < Paper
                 elevation={20}
                 sx={{
@@ -22,10 +61,12 @@ export const SignUp = (props) => {
                     margin="normal"
                     required
                     fullWidth
-                    name="username"
-                    label="Username"
-                    type="username"
                     id="Username"
+                    label="Username"
+                    name="username"
+                    type="username"
+                    value={formState.name}
+                    onChange={handleChange}
                 />
                 <TextField
                     margin="normal"
@@ -35,24 +76,32 @@ export const SignUp = (props) => {
                     label="Email Address"
                     name="email"
                     type="email"
+                    value={formState.email}
+                    onChange={handleChange}
                 />
                 <TextField
                     margin="normal"
                     required
                     fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
                     id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    autoComplete='false'
+                    value={formState.password}
+                    onChange={handleChange}
                 />
                 <TextField
                     margin="normal"
                     required
                     fullWidth
-                    name="confirm-password"
-                    label="Confirm Password"
-                    type="password"
                     id="confirm-password"
+                    label="Confirm Password"
+                    name="confirm-password"
+                    type="password"
+                    autoComplete='false'
+                    // value={formState.confirm_password}
+                    // onChange={handleChange}
                 />
                 <Button style={{ padding: 0 }} color='inherit'>
                     <Typography sx={{ fontSize: '16px', textTransform: 'none', m: '9px', mr: '0px' }} onClick={() => { props.setIsLoginScreen(!props.isLoginScreen) }}>Have an account?</Typography>
@@ -63,6 +112,7 @@ export const SignUp = (props) => {
                     variant="contained"
                     color="secondary"
                     sx={{ mt: 3, mb: 2 }}
+                    onClick={(event) => { handleFormSubmit(event) }}
                 >
                     Sign In
                 </Button>
