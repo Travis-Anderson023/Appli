d404a1650c46fbdeb97e8f878f7800a459295cba
Travis-Anderson023 <tsanderson.023@gmail.com>
2022-05-04 11:21:46 -0400

    nested deploy working

--- client/src/components/misc/Nav.js
+++ client/src/components/misc/Nav.js
@@ -1,77 +1,128 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import HomeIcon from '@mui/icons-material/Home';
-import LogoutIcon from '@mui/icons-material/Logout';
-import { BottomNavigation, BottomNavigationAction, CardMedia, MenuItem, Typography } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
-import IconButton from '@mui/material/IconButton';
-import Toolbar from '@mui/material/Toolbar';
-import { useNavigate } from 'react-router-dom';
-import logoTextBlack from '../../assets/logo-textBlack.png';
-import logoTextWhite from '../../assets/logo-textWhite.png';
-import auth from '../../utils/auth';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ContentPasteIcon from "@mui/icons-material/ContentPaste";
+import HomeIcon from "@mui/icons-material/Home";
+import LogoutIcon from "@mui/icons-material/Logout";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  CardMedia,
+  MenuItem,
+  Typography,
+} from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Toolbar from "@mui/material/Toolbar";
+import { useNavigate } from "react-router-dom";
+import logoTextBlack from "../../assets/logo-textBlack.png";
+import logoTextWhite from "../../assets/logo-textWhite.png";
+import auth from "../../utils/auth";
 
 export const Nav = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const styles = {
-        botNavAction: {
-            color: 'text.primary',
-            '&& .Mui-selected': {
-                color: 'text.primary',
-            }
-        }
-    }
+  const styles = {
+    botNavAction: {
+      color: "text.primary",
+      "&& .Mui-selected": {
+        color: "text.primary",
+      },
+    },
+  };
 
-    if (props.isSmOrUp) {
-        return (
-            <AppBar position="static" color='primary' sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Toolbar variant="dense">
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu" sx={{ mr: 2 }}
-                        onClick={() => window.location = '/'}
-                    >
-                        <CardMedia
-                            component='img'
-                            src={logoTextWhite}
-                            sx={{
-                                height: '30px'
-                            }}
-                        />
-                    </IconButton>
-                    <MenuItem onClick={() => { navigate('/applications') }}>
-                        <Typography textAlign="center" sx={{color:"white"}}>Applications</Typography>
-                    </MenuItem>
-                    <MenuItem onClick={() => { navigate('/account') }}>
-                        <Typography textAlign="center" sx={{color:"white"}}>Account</Typography>
-                    </MenuItem>
-                </Toolbar>
-                <MenuItem onClick={() => auth.logout()}>
-                    <Typography textAlign="center">Logout</Typography>
-                </MenuItem>
-            </AppBar>
-        )
-    } else {
-        return (
-            <BottomNavigation
-                sx={{ bgcolor: 'primary.main', height: '65px' }}
-                showLabels
-                color='primary'
-                value={props.page}
-                onChange={(event, newValue) => {
-                    //dont want logout to have a page value
-                    if (newValue !== 3) {
-                        props.setPage(newValue);
-                    }
-                }}
-            >
-                <BottomNavigationAction sx={{ ...styles.botNavAction, transform: 'scale(85%)' }} label="Home" icon={<HomeIcon sx={styles.botNavAction} />} onClick={() => navigate('/')} />
-                <BottomNavigationAction sx={{ ...styles.botNavAction, transform: 'scale(85%)' }} label="Applications" icon={<ContentPasteIcon sx={styles.botNavAction} />} onClick={() => { navigate('/applications') }} />
-                <BottomNavigationAction sx={{ ...styles.botNavAction, transform: 'scale(85%)' }} label="Account" icon={<AccountCircleIcon sx={styles.botNavAction} />} onClick={() => { navigate('/account') }} />
-                <BottomNavigationAction sx={{ ...styles.botNavAction, transform: 'scale(85%)' }} label="Logout" icon={<LogoutIcon sx={styles.botNavAction} />} onClick={() => auth.logout()} />
-            </BottomNavigation >
-        )
-    }
-}
+  if (props.isSmOrUp) {
+    return (
+      <AppBar
+        position="static"
+        color="primary"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Toolbar variant="dense">
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => (window.location = "/")}
+          >
+            <CardMedia
+              component="img"
+              src={logoTextWhite}
+              sx={{
+                height: "30px",
+              }}
+            />
+          </IconButton>
+          <MenuItem
+            onClick={() => {
+              navigate("/applications");
+            }}
+          >
+            <Typography textAlign="center" sx={{ color: "white" }}>
+              Applications
+            </Typography>
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              navigate("/account");
+            }}
+          >
+            <Typography textAlign="center" sx={{ color: "white" }}>
+              Account
+            </Typography>
+          </MenuItem>
+        </Toolbar>
+        <MenuItem onClick={() => auth.logout()}>
+          <Typography textAlign="center">Logout</Typography>
+        </MenuItem>
+      </AppBar>
+    );
+  } else {
+    return (
+      <BottomNavigation
+        sx={{ bgcolor: "primary.main", height: "65px" }}
+        showLabels
+        color="primary"
+        value={props.page}
+        onChange={(event, newValue) => {
+          //dont want logout to have a page value
+          if (newValue !== 3) {
+            props.setPage(newValue);
+          }
+        }}
+      >
+        <BottomNavigationAction
+          sx={{ ...styles.botNavAction, transform: "scale(85%)" }}
+          label="Home"
+          icon={<HomeIcon sx={styles.botNavAction} />}
+          onClick={() => navigate("/")}
+        />
+        <BottomNavigationAction
+          sx={{ ...styles.botNavAction, transform: "scale(85%)" }}
+          label="Applications"
+          icon={<ContentPasteIcon sx={styles.botNavAction} />}
+          onClick={() => {
+            navigate("/applications");
+          }}
+        />
+        <BottomNavigationAction
+          sx={{ ...styles.botNavAction, transform: "scale(85%)" }}
+          label="Account"
+          icon={<AccountCircleIcon sx={styles.botNavAction} />}
+          onClick={() => {
+            navigate("/account");
+          }}
+        />
+        <BottomNavigationAction
+          sx={{ ...styles.botNavAction, transform: "scale(85%)" }}
+          label="Logout"
+          icon={<LogoutIcon sx={styles.botNavAction} />}
+          onClick={() => auth.logout()}
+        />
+      </BottomNavigation>
+    );
+  }
+};
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,4 +1,4 @@
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
 
 class AuthService {
   getProfile() {
@@ -13,24 +13,24 @@ class AuthService {
   isTokenExpired(token) {
     const decoded = decode(token);
     if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem('id_token');
+      localStorage.removeItem("id_token");
       return true;
     }
     return false;
   }
 
   getToken() {
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/applications');
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/appli/applications");
   }
 
   logout() {
-    localStorage.removeItem('id_token');
-    window.location.assign('/');
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
     // window.location.reload();
   }
 }
